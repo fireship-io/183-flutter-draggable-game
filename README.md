73db9a2718255584cd72802f46d46f61ed65667b
codediodeio <hello@jeffdelaney.me>
2019-04-30 12:55:34 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Episode 183 - Flutter Drag and Drop
+
+Create a drag-and-drop game for kids with [Flutter](https://fireship.io/tags/flutter). 
+
+![](https://firebasestorage.googleapis.com/v0/b/fireship-app.appspot.com/o/assets%2Fdemo-kids-game.gif?alt=media&token=6d57a2d0-808e-4bb5-b7c2-2cb4ebbf9823)
---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'PressStart',
+      ),
+      home: ColorGame(),
+    );
+  }
+}
+
+class ColorGame extends StatefulWidget {
+  ColorGame({Key key}) : super(key: key);
+
+  createState() => ColorGameState();
+}
+
+class ColorGameState extends State<ColorGame> {
+  /// Map to keep track of score
+  final Map<String, bool> score = {};
+
+  /// Choices for game
+  final Map choices = {
+    '🍏': Colors.green,
+    '🍋': Colors.yellow,
+    '🍅': Colors.red,
+    '🍇': Colors.purple,
+    '🥥': Colors.brown,
+    '🥕': Colors.orange
+  };
+
+  // Random seed to shuffle order of items.
+  int seed = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Score ${score.length} / 6'),
+          backgroundColor: Colors.pink),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            score.clear();
+            seed++;
+          });
+        },
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: choices.keys.map((emoji) {
+                return Draggable<String>(
+                  data: emoji,
+                  child: Emoji(emoji: score[emoji] == true ? '✅' : emoji),
+                  feedback: Emoji(emoji: emoji),
+                  childWhenDragging: Emoji(emoji: '🌱'),
+                );
+              }).toList()),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+                choices.keys.map((emoji) => _buildDragTarget(emoji)).toList()
+                  ..shuffle(Random(seed)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDragTarget(emoji) {
+    return DragTarget<String>(
+      builder: (BuildContext context, List<String> incoming, List rejected) {
+        if (score[emoji] == true) {
+          return Container(
+            color: Colors.white,
+            child: Text('Correct!'),
+            alignment: Alignment.center,
+            height: 80,
+            width: 200,
+          );
+        } else {
+          return Container(color: choices[emoji], height: 80, width: 200);
+        }
+      },
+      onWillAccept: (data) => data == emoji,
+      onAccept: (data) {
+        setState(() {
+          score[emoji] = true;
+          plyr.play('success.mp3');
+        });
+      },
+      onLeave: (data) {},
+    );
+  }
+}
+
+class Emoji extends StatelessWidget {
+  Emoji({Key key, this.emoji}) : super(key: key);
+
+  final String emoji;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        padding: EdgeInsets.all(10),
+        child: Text(
+          emoji,
+          style: TextStyle(color: Colors.black, fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
+
+AudioCache plyr = AudioCache();
